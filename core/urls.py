a91f0f432b915d8c38bc273b69f7cc11e3372016
Lorena <zolsslorena@gmail.com>
2023-11-07 23:44:35 -0300

    first commit

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact', views.contact, name='contact'),
+    path('contact2', views.contact2, name='contact2'),
+    path('details/<int:producto_id>', views.details, name='details'),
+    path('nosotros', views.nosotros, name='nosotros'),
+]
